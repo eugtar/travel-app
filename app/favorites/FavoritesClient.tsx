95b5cf515d7c4a806fb1de9407a7028bf303d445
eugtar <112690358+eugtar@users.noreply.github.com>
2023-07-16 23:01:25 +0300

    fix

--- app/favorites/FavoritesClient.tsx
+++ app/favorites/FavoritesClient.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import { TSafeListing, TSafeUser } from "../types";
 import { Container, Heading } from "../components";
--- app/reservations/ReservationsClient.tsx
+++ app/reservations/ReservationsClient.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import { Container, Heading } from "../components";
 import { TSafeReservation, TSafeUser } from "../types";
